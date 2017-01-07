a564202f7d9bccf587f595b182ab872100e1b484
Luc Neville <xsintill@gmail.com>
2017-01-07 15:06:21 +0100

    -removed use strict since that is autogenerated since typescript 2.1 with alwaysstrict set to true -fixed al tslint warnings -changed angular 2 references to angular 1

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,25 @@
-'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as changeCase from 'change-case';
-import { Observable } from 'rxjs';
+import * as vscode from "vscode";
+import * as changeCase from "change-case";
+import { Observable } from "rxjs";
 
-import { FileHelper } from './FileHelper';
-import { Config } from './config.interface';
+import { FileHelper } from "./FileHelper";
+import { Config } from "./config.interface";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
     // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
+    // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.genAngular2ComponentFiles', (uri) => {
+    let disposable = vscode.commands.registerCommand("extension.genAngular1ComponentFiles", (uri) => {
         // The code you place here will be executed every time your command is executed
 
-        let configPrefix: String = 'ng2ComponentGenerator';
+        let configPrefix: String = "ng1ComponentGenerator";
         let _workspace = vscode.workspace;
-        let config = <Config>_workspace.getConfiguration((configPrefix + '.config'));
+        let config = <Config>_workspace.getConfiguration((configPrefix + ".config"));
 
         if (!config.files) {
             config = FileHelper.getConfig();
@@ -29,14 +28,14 @@ export function activate(context: vscode.ExtensionContext) {
         // Display a dialog to the user
         let enterComponentNameDialog$ = Observable.from(
             vscode.window.showInputBox(
-                {prompt: 'Please enter component name in camelCase'}
+                {prompt: "Please enter component name in camelCase"}
             ));
-
+            
 
         enterComponentNameDialog$
             .concatMap( val => {
                     if (val.length === 0) {
-                        throw new Error('Component name can not be empty!');
+                        throw new Error("Component name can not be empty!");
                     }
                     let componentName = changeCase.paramCase(val);
                     let componentDir = FileHelper.createComponentDir(uri, componentName);
@@ -55,17 +54,17 @@ export function activate(context: vscode.ExtensionContext) {
             .concatMap(filename => Observable.from(vscode.workspace.openTextDocument(filename)))
             .concatMap(textDocument => {
                 if (!textDocument) {
-                    throw new Error('Could not open file!');
+                    throw new Error("Could not open file!");
                 };
-                return Observable.from(vscode.window.showTextDocument(textDocument))
+                return Observable.from(vscode.window.showTextDocument(textDocument));
             })
             .do(editor => {
                 if (!editor) {
-                    throw new Error('Could not open file!')
+                    throw new Error("Could not open file!");
                 };
             })
             .subscribe(
-                () => vscode.window.setStatusBarMessage('Component Successfuly created!'),
+                () => vscode.window.setStatusBarMessage("Component Successfuly created!"),
                 err => vscode.window.showErrorMessage(err.message)
             );
 
