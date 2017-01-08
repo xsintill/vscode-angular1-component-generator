4ddbd9e4b63a9c89413b2839aec34a64d4929916
Luc Neville <xsintill@gmail.com>
2017-01-08 08:23:09 +0100

    added the new config items to the interface

--- src/config.interface.ts
+++ src/config.interface.ts
@@ -1,7 +1,37 @@
-import { WorkspaceConfiguration } from 'vscode';
+import { WorkspaceConfiguration } from "vscode";
 
 export interface Config extends WorkspaceConfiguration {
     files: {
+        filter: {
+            create: boolean,
+            extension: string,
+            template?: string
+        },
+        controller: {
+            create: boolean,
+            extension: string,
+            template?: string
+        },
+        directive: {
+            create: boolean,
+            extension: string,
+            template?: string
+        },
+        configRoute: {
+            create: boolean,
+            extension: string,
+            template?: string
+        },
+        testFile: {
+            create: boolean,
+            extension: string,
+            template?: string
+        },
+        service: {
+            create: boolean,
+            extension: string,
+            template?: string
+        },
         component: {
             create: boolean,
             extension: string,
