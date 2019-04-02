9f8134aa2c6231fc718d4448fb44a1c505ffa0bf
Luc Neville <lneville@learnlinq.nl>
2019-04-02 16:11:29 +0200

    changed the install-local.bat to also copy to the vscode insiders dir

--- install-local.bat
+++ install-local.bat
@@ -1,2 +1,3 @@
 xcopy /s /Y . %USERPROFILE%\.vscode\extensions\angular-file-generator\ /EXCLUDE:exclude.txt
+xcopy /s /Y . %USERPROFILE%\.vscode-insiders\extensions\angular-file-generator\ /EXCLUDE:exclude.txt
 
