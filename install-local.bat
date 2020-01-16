60c5c3f47ec7946e08e721ca96b3dd378e201eab
Luc Neville <lneville@learnlinq.nl>
2020-01-16 09:34:23 +0100

    closes #66 add a npm install after copy

--- install-local.bat
+++ install-local.bat
@@ -1,3 +1,7 @@
 xcopy /s /Y . %USERPROFILE%\.vscode\extensions\angular-file-generator\ /EXCLUDE:exclude.txt
 xcopy /s /Y . %USERPROFILE%\.vscode-insiders\extensions\angular-file-generator\ /EXCLUDE:exclude.txt
+cd %USERPROFILE%\.vscode\extensions\angular-file-generator\
+npm install
+cd %USERPROFILE%\.vscode-insiders\extensions\angular-file-generator\
+npm install
 
