2d5adf3511181b6ff49fc9547a96d873d2922bde
Luc Neville <lneville@learnlinq.nl>
2020-03-24 16:01:29 +0100

    build the sources after npm install so the extension can be used after using local-install.bat

--- install-local.bat
+++ install-local.bat
@@ -2,6 +2,8 @@ xcopy /s /Y . %USERPROFILE%\.vscode\extensions\angular-file-generator\ /EXCLUDE:
 xcopy /s /Y . %USERPROFILE%\.vscode-insiders\extensions\angular-file-generator\ /EXCLUDE:exclude.txt
 cd %USERPROFILE%\.vscode\extensions\angular-file-generator\
 npm install
+tsc -p ./
 cd %USERPROFILE%\.vscode-insiders\extensions\angular-file-generator\
 npm install
+tsc -p ./
 
