0b0856ae5027802ea36b05a18d2f11a03a0643bd
Luc Neville <lneville@learnlinq.nl>
2020-03-25 08:41:32 +0100

    prevent npm install exiting from batch so sequent statements will be executed

--- install-local.bat
+++ install-local.bat
@@ -1,9 +1,9 @@
 xcopy /s /Y . %USERPROFILE%\.vscode\extensions\angular-file-generator\ /EXCLUDE:exclude.txt
 xcopy /s /Y . %USERPROFILE%\.vscode-insiders\extensions\angular-file-generator\ /EXCLUDE:exclude.txt
 cd %USERPROFILE%\.vscode\extensions\angular-file-generator\
-npm install
-tsc -p ./
+call npm install
+call tsc -p ./
 cd %USERPROFILE%\.vscode-insiders\extensions\angular-file-generator\
-npm install
-tsc -p ./
+call npm install
+call tsc -p ./
 
